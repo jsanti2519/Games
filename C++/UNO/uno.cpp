187fffebe636b0647d415388d65efdb0da229e17
jsanti2519 <53867046+jsanti2519@users.noreply.github.com>
2020-02-06 11:06:18 -0500

    Update uno.cpp
    This is still an unfinished UNO game. I took a pause in completing the project, as my schedule prevented me over the winter break. I debugged the program for the most part.

--- C++/UNO/uno.cpp
+++ C++/UNO/uno.cpp
@@ -1,189 +1,175 @@
+/*if flip = 0, then that's your turn (heads). If flip = 1, then that's the computer's turn (tails). */
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <string>
 using namespace std;
 
-string deck[108], shuffle[108], yourDeck[100], pcDeck[100], cardDrop[108];
-string colors[4] = {"Red", "Yellow", "Blue", "Green"};
-string numbers[12] = {" 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9", " +2", " Skip", " Reverse"};
+string deck[200], shuffle[200], cardDrop[200], yourDeck[100], pcDeck[100];
+string colors[] = {"Red", "Blue", "Green", "Yellow"};
+string numbers[] = {" 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9"};
+string addOn[] = {" Skip", " +2", " Reverse"};
+string wild[] = {"Wild +4", "Wild Card"};
 string zero = " 0";
-string WildCards[2] = {"Wild +4", "Wild Card"};
-int index[108], shuffleIndex[108];
-int cardDropCount = 0, yourDeckCount = 7, pcDeckCount = 7;
-bool turns;
+int index[200], shuffleIndex[200], cardDropCount = 0, yourDeckCount = 0, pcDeckCount = 0;
 
-void setUp();
-void shuffleDeck();
-void initialDraw(int&, int);
-void removeElement(string[], int, int);
+void setUp() {
+   int colorCount = 0, numCount = 0, trial = 0;
+   for(int i = 0; i < 72; i++) {
+      deck[i] = colors[colorCount] + numbers[numCount++];
+      if(numCount == 9 && trial != 1) {
+         numCount = 0;
+         trial = 1;
+      }
+      else if(numCount == 9 && trial == 1) {
+         numCount = 0;
+         colorCount++; 
+	 trial = 0;
+      }
+      index[i] = i;
+   }
 
+   colorCount = 0;
+
+   for(int i = 72; i < 76; i++) {
+      deck[i] = colors[colorCount++] + zero; //adding the zeros for every color.
+      index[i] = i;
+   }
+
+   colorCount = 0; numCount = 0; trial = 0;
+
+   for(int i = 76; i < 100; i++) { //there's two of each action card. That's why "trial" is implemented.
+      deck[i] = colors[colorCount] + addOn[numCount++];
+      if(numCount == 3 && trial == 0) {
+         numCount = 0;
+         trial = 1;
+      }
+      else if(numCount == 3 && trial == 1) {
+         numCount = 0;
+         colorCount++;
+         trial = 0;
+      }
+      index[i] = i;
+   }
+
+   for(int i = 100; i < 108; i++) {
+      if(i <= 103) deck[i] = wild[0];
+      else deck[i] = wild[1];
+      index[i] = i;
+   }
+}
+
+void shuffleDeck() {
+  shuffleIndex[0] = index[rand() % 108]; //Each index is different. But certain strings are the same property. That's why index gets randomized.
+  for(int i = 1; i < 108; i++) {
+    bool repeats = true;
+    while(repeats) {
+      shuffleIndex[i] = index[rand() % 108];
+      for(int k = 0; k < i; k++) {
+        if(shuffleIndex[k] == shuffleIndex[i]) {
+          repeats = true;
+          break;
+        }
+        repeats = false;
+      }
+    }
+  }
+  for(int i = 0; i < 108; i++) shuffle[i] = deck[shuffleIndex[i]];
+}
+
+int count = 107;
+
+void initialDraw() {
+  cout << "This is your set: \n\n";
+
+  int i = 0;
+  while(i < 7) {
+     yourDeck[i] = shuffle[count];
+     shuffle[count] = "";
+     cout << i + 1 << "). " << yourDeck[i] << endl;
+     i++; count--;
+     yourDeckCount++; //this will keep track of the size of your deck. Same goes with pc's deck.
+  }
+
+  i = 0;
+  while(i < 7) {
+     pcDeck[i] = shuffle[count];
+     shuffle[count] = "";
+     i++; count--;
+     pcDeckCount = i + 1; //keeps track of size of PC's deck
+  }
+}
+
+int flip;
+int choice = -1; //in case it's your turn.
+
+void firstTurn() {
+  cout << endl << endl << "Flip a coin: (Press ENTER to flip)";
+  cin.get();
+  flip = rand() % 2;
+
+//PC's turn.
+  if(flip == 1) {
+    cout << "Tails. PC starts.";
+    int n = rand() % 7;
+    for(int i = 0; i < pcDeckCount; i++) {
+      if(pcDeck[i] == pcDeck[n]) {
+        pcDeck[i] = pcDeck[i + 1];
+        pcDeckCount--;
+      }
+    }
+    cardDrop[cardDropCount++] = pcDeck[n]; 
+  }
+
+//Your turn.
+  else {
+    cout << "Heads. You start. Select a number. " << endl;
+    while(choice < 1 || choice > yourDeckCount) cin >> choice;
+
+    cardDrop[cardDropCount++] = yourDeck[choice - 1];
+    for(int i = 0; i < yourDeckCount; i++) {
+      if(yourDeck[i] == yourDeck[choice - 1]) {
+        yourDeck[i] = yourDeck[i + 1];
+      }
+    }
+    yourDeckCount--;
+  }
+}
+
+void yourTurn() { //Not finished yet
+  cout << endl << endl << "The card placed on the table is " << cardDrop[cardDropCount - 1] << ". ";
+
+  flip = 0; //indicates that the PC will go afterwards.
+  choice = -1;
+  cout << "Here are your cards. Select a number. (Type 0 to draw)" << endl << endl;
+  for(int i = 0; i < yourDeckCount; i++) cout << i + 1 << "). " << yourDeck[i] << endl;
+  while(choice < 0 || choice > yourDeckCount) cin >> choice;
+  if(choice == 0) {
+    yourDeck[yourDeckCount++] = shuffle[count--]; //yourDeckCount is after the maximum index position.
+    cout << endl << endl << "You drew. Here are your new sets. " << endl << endl;
+    for(int i = 0; i < yourDeckCount; i++) cout << i + 1 << "). " << yourDeck[i] << endl;
+  }
+  else {
+    cardDrop[cardDropCount++] = yourDeck[choice - 1];
+    for(int i = 0; i < yourDeckCount; i++) {
+      if(yourDeck[i] == yourDeck[choice - 1]) yourDeck[i] = yourDeck[i + 1];
+      yourDeck[yourDeckCount - 1] = ""; //the last index position
+    }
+    yourDeckCount--;
+  }
+  cout << endl << endl << cardDrop[cardDropCount - 1] << " is on the table. Here are your new sets. \n";
+  for(int i = 0; i < yourDeckCount; i++) cout << i + 1 << "). " << yourDeck[i] << endl;
+}
 
 int main() {
   srand(time(NULL));
   setUp();
   shuffleDeck();
-  
-  int selection;
-  int max;
-  cout << "Press 0 to Draw 7: "; // fix this. I can type 3 and still proceeds.
-  cin >> selection;
-  initialDraw(max, selection);
+  initialDraw();
+  firstTurn();
+  if(flip == 1) yourTurn();
+ // else pcTurn();
 
-  while(pcDeckCount > 0 || yourDeckCount > 0) {
-    int baz = cardDrop[cardDropCount].find(" ");
-    if(!turns) { //that means it's the computer's turn.
-      for(int j = 0; j < pcDeckCount; j++) {
-        int waldo = pcDeck[j].find(" "); 
-        if(pcDeck[j] == "Wild +4" || pcDeck[j] == "Wild Card") {
-          effects(turns); turns = true; pcDeckCount--; cardDropCount++;
-          break;
-        }
-        //Line below in English: If color matches with color OR if number matches with number.
-        else if((pcDeck[j].substr(0, waldo) == cardDrop[cardDropCount].substr(0, baz) || (pcDeck[j].substr(waldo, 2) == cardDrop[cardDropCount].substr(baz, 2)))) {
-          if(pcDeck[j].substr(waldo, 2) == " +2") { //if color matches and it's a +2.
-            for(int k = 0; k < 2; k++) yourDeck[yourDeckCount++] = shuffle[max--];
-            pcDeckCount--; cardDropCount++; turns = true; //PC drops the +2 card. Your turn.
-            break; 
-          }
-          else if(pcDeck[j].substr(waldo, 7) == " Reverse" || pcDeck[j].substr(waldo, 5) == " Skip") {
-            pcDeckCount--; cardDropCount++; turns = false; //PC drops the reverse/skip card. PC goes again.
-            break;
-          }
-          else {
-            cardDrop[cardDropCount] = pcDeck[j];
-            pcDeckCount--; cardDropCount++; turns = true;
-            break;
-          }
-        } // matching colors/numbers else if bracket.
-      } // for loop bracket.
-    } // if(false) bracket.
-    if(pcDeckCount == 1) cout << "PC: UNO!";
-    
-    if(turns) {
-      cout << "Here are your sets. Choose one: \n";
-      for(int i = 0; i < yourDeckCount; i++) {
-        cout << i + 1 << "). " << yourDeck[i] << endl;
-      }
-      cin >> selection; selection--;
-      int fred = yourDeck[selection].find(" "); 
-      if(yourDeck[selection] == "Wild Card" || yourDeck[selection] == "Wild +4") {
-        effects(!turns); yourDeckCount--; cardDropCount++; turns = false;
-      }
-      else if(yourDeck[selection].substr(0, fred) == cardDrop[cardDropCount].substr(0, baz) || yourDeck[selection].substr(fred, 2) == cardDrop[cardDropCount].substr(baz, 2) {
-        if(yourDeck[selection].substr(fred, 2) == " +2") {
-          for(int k = 0; k < 2; k++) pcDeck[pcDeckCount++] = shuffle[max--];
-          yourDeckCount--; cardDropCount++; turns = false;
-        }
-        else if(yourDeck[selection].substr(fred, 2) == " Reverse" || yourDeck[selection].substr(fred, 2) == " Skip") {
-          yourDeckCount--; cardDropCount++; turns = true; //You go again.
-        }
-        else {
-          cardDrop[cardDropCount] = yourDeck[selection];
-          yourDeckCount--; cardDropCount++; turns = false;
-        } //inner else bracket
-      } //outer else if bracket
-    } // true / false bracket
-    if(yourDeckCount == 1) {
-      string uno;
-      cout << "Type UNO";
-      cin >> uno;
-      if(uno != "UNO" || uno != "uno") yourDeck[++yourDeckCount] == shuffle[max--];
-    }
-        
-  } //while loop bracket
-  if(pcDeckCount == 0) cout << "PC: UNO OUT! You lose";
-  else cout << "Uno out! You win!";
-              
   return 0;
-        
-}
-
-void setUp() {
-  int colorCount = 0, numCount = 0, twice = 1;
-  for(int i = 0; i < 96; i++) {
-    deck[i] = colors[colorCount] + numbers[numCount++];
-    if(numCount == 12 && twice == 2) {
-      colorCount++; numCount = 0; twice = 1;
-    }
-    else if(numCount == 12) {
-      numCount = 0; twice++;
-    }
-  }
-  colorCount = 0; //resetting the count to zero for colors[].
-  for(int j = 96; j < 100; j++) {
-    deck[j] = colors[colorCount++] + zero;
-  }
-  for(int k = 100; k < 108; k++) {
-    if(k < 104) deck[k] = WildCards[0];
-    else deck[k] = WildCards[1];
-  }
-}
-
-void shuffleDeck() {
-  for(int i = 0; i < 108; i++) index[i] = i;
-  shuffleIndex[0] = index[rand() % 108];
-  for(int j = 1; j < 108; j++) {
-    bool repeats = true;
-    while(repeats) {
-      shuffleIndex[j] = index[rand() % 108];
-      for(int k = 0; k < j; k++) {
-        if(shuffleIndex[k] == shuffleIndex[j]) {
-          repeats = true; break;
-        }
-        else repeats = false;
-      }
-    }
-  }
-    for(int l = 0; l < 108; l++) shuffle[l] = deck[shuffleIndex[l]];
-}
-
-void initialDraw(int &foo, int &bar) { //foo linked with max in main(), while bar is linked with selection variable.
-  cout << "Your sets are: \n";
-  for(int i = 0; i < yourDeckCount; i++) {
-    foo = 107 - i;
-    yourDeck[i] = shuffle[foo];
-    cout << i + 1 << "). " << yourDeck[i] << "\n";
-    removeElement(shuffle, 108, foo); //removes element in position where max is.
-  }
-  
-  for(int j = 0; j < pcDeckCount; j++) { //Computer's initial draw.
-    foo = 100 - j;
-    pcDeck[j] = shuffle[foo];
-    removeElement(shuffle, 108, foo);
-  }
-
-  cout << "\n\nSelect a number to draw: "; //your first turn.
-  cin >> bar; bar--; cout << "\n\n";
-  for(int k = 0; k < yourDeckCount; k++) {
-    if(k >= bar) yourDeck[k] = yourDeck[k + 1];
-  }
-  cardDrop[cardDropCount++] = yourDeck[bar];
-  turns = false;
-}
-
-void effects(int &qux) { //linked with max.
-  int n = rand() % pcDeckCount;
-  int randColors = rand() % 4;
-  for(int i = 0; i < pcDeckCount; i++) {
-    cardDrop[cardDropCount] = pcDeck[n]; // necessary??
-    if(pcDeck[n] == "Wild +4") {
-      cout << "I dropped a Wild + 4. I'm picking " << colors[randColors];
-      cardDrop[cardDropCount] = colors[randColors]; cardDropCount++;
-      for(int j = 0; j < yourDeckCount + 4; j++) { //double check this again.
-        yourDeck[yourDeckCount + j + 1] = deck[qux];
-        qux--;
-      }
-      break;
-    }
-    else if(pcDeck[n] == "Wild Card") {
-      cout << "I dropped a Wild Card: I'm picking " << colors[randColors];
-      cardDrop[cardDropCount] = colors[randColors]; cardDropCount++;
-      break;
-    }
-  }
-
-void removeElement(string a[], int cap, int pos) {
-  for(int i = 0; i < cap; i++) {
-    if(i >= pos) a[i] = " ";
-  }
 }
